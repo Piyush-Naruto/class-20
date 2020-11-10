66464e790422c2f4048672067ea9bce64257da3f
Piyush-Naruto <73929580+Piyush-Naruto@users.noreply.github.com>
2020-11-10 15:13:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,43 @@
-var movingRect,fixedRect;
+var fixedRect, movingRect;
 
 function setup() {
-  createCanvas(800,400);
-   movingRect = createSprite(400, 200, 50, 50);
-   movingRect.shapeColor = "green";
-   movingRect.debug = "true"
-   fixedRect = createSprite(400, 200, 50, 50);
-   fixedRect.shapeColor = "green"; 
-   fixedRect.debug = "true"
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  gameobject1 = createSprite(100,50,50,50);
+  gameobject1.shapeColor = "green";
+  gameobject2 = createSprite(200,50,50,50);
+  gameobject2.shapeColor = "green";
+  gameobject3 = createSprite(300,50,50,50);
+  gameobject3.shapeColor = "green";
+  gameobject4 = createSprite(400,50,50,50);
+  gameobject4.shapeColor = "green";
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background(255,255,255);  
-  movingRect.x = World.mouseX
-  movingRect.y = World.mouseY
+  background(0,0,0);  
 
-  if(movingRect.x - fixedRect.x < movingRect.width/2 +fixedRect.width/2 && 
-    fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2){
-    movingRect.shapeColor = "red";
-    fixedRect.shapeColor = "red";
-  }
-  else{
-     movingRect.shapeColor = "green";
-     fixedRect.shapeColor = "green"; 
+  bounceoff(movingRect,fixedRect);
+
+  drawSprites();}
+
+  function bounceoff(object1,object2){
+    if (object1.x - object2.x < object2.width/2 + object1.width/2
+      && object2.x - object1.x < object2.width/2 + object1.width/2) {
+        object1.velocityX = object1.velocityX * (-1);
+        object2.velocityX = object2.velocityX * (-1);
     }
-  drawSprites();
-}
+    if (object1.y - object2.y < object2.height/2 + object1.height/2
+     && object2.y - object1.y < object2.height/2 + object1.height/2){
+      object1.velocityY = object1.velocityY * (-1);
+      object2.velocityY = object2.velocityY * (-1);
+  }
+  }
